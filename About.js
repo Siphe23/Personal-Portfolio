8936c383cbfa6f1a00665d34a426dd6302c18eb6
Siphe23 <siphesihlemzamane@gmai.com>
2024-07-17 21:56:38 +0200

    commit

---
+++ About.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const hireMeButtons = document.querySelectorAll(".hire-me");
+
+    hireMeButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            alert("Thank you for your interest! Please contact me at Siphesihlemzamane@gmail.com.");
+        });
+    });
+});
